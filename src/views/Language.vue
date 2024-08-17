6c30818b9acbc9bbeb53220181eebdd7ba4ea5f9
P-Kuszyk <p.kuszyk1221@wp.pl>
2024-08-17 12:42:30 +0200

    Add Language page

--- src/views/Language.vue
+++ src/views/Language.vue
@@ -4,7 +4,11 @@
     <section class="section">
       <div class="container">
         <h1 class="title">Język</h1>
-        <p>Pozdrooo</p>
+        <div v-if="loading">Ładowanie...</div>
+        <div v-if="error" class="error">Wystąpił błąd: {{ error }}</div>
+        <ul v-if="words.length">
+          <li v-for="word in words" :key="word.id">{{ word.word }} -> {{ word.translatedWord }}</li>
+        </ul>
       </div>
     </section>
   </div>
@@ -12,10 +16,34 @@
 
 <script>
 import Navbar from '@/components/Navbar.vue'
+import axios from 'axios'
 
 export default {
   components: {
     Navbar
+  },
+  data() {
+    return {
+      words: [],
+      loading: true,
+      error: null
+    }
+  },
+  async created() {
+    try {
+      const response = await axios.get('http://localhost:5000/api/words')
+      this.words = response.data
+    } catch (err) {
+      this.error = err.message
+    } finally {
+      this.loading = false
+    }
   }
 }
 </script>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
